6ef183019cf3ddd4764dc94c83bdd6d46d2a992e
Dilek BAKAR <108486801+dilekbakar@users.noreply.github.com>
2022-08-10 14:35:31 +0300

    commit
    singalR chat app

---
+++ BlazorChatSignalR/BlazorChatSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatSignalR.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("","Bir Kullanıcı Bağlandı!");
+            await base.OnConnectedAsync();
+
+        }
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
